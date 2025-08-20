e5e4a2a40f0e2bcaf4d033bab19edf9323679202
Salman Rakin <salmankaderrakin@gmail.com>
2025-08-20 15:01:54 +0600

    Updates PostCSS and Tailwind CSS configuration.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
-// This configuration file sets up Tailwind CSS for a Vue.js project, specifying where to look for class names and extending the default theme if needed.   
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
-  theme: { extend: {} },
+  safelist: [
+      'text-green-400',
+      'text-yellow-400',
+      'text-pink-400',
+      'text-gray-400',
+      'italic'
+    ],
+
+  theme: {
+    extend: {},
+  },
   plugins: [],
 }
