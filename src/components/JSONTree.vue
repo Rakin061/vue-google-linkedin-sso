4b3b80006148ef187153af80da5855ffbc9d493c
Salman Rakin <salmankaderrakin@gmail.com>
2025-08-21 10:06:18 +0600

    Long text in json wrapped to the container widht in stead of special characters..

--- src/components/JSONTree.vue
+++ src/components/JSONTree.vue
@@ -55,7 +55,7 @@
 
     <!-- Primitive -->
     <template v-else>
-      <span :class="valueClass">{{ formattedValue }}</span>
+      <span :class="[valueClass,'break-all']">{{ formattedValue }}</span>
     </template>
   </div>
 </template>
